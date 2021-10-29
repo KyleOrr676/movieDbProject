c0c89e1b48edf102a8b2639589e09364c7b376a9
Kyle Orr <KORR202@caledonian.ac.uk>
2021-10-29 14:57:18 +0100

    Repository for movie database project

---
+++ src/test/java/com/institute/software/the/orr/kyle/moviesDB/demo/MoviesDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.institute.software.the.orr.kyle.moviesDB.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
